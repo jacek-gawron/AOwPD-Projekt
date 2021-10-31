a870bc4925eb8bf2811c753c55806b46383a02a2
Bartosz-Panus <248841@student.pwr.edu.pl>
2021-11-01 00:52:20 +0100

    Added Matrix class

---
+++ Matrix/Matrix/utils/matrix/Matrix.cpp
@@ -0,0 +1,75 @@
+#include "./Matrix.hpp"
+#include <iostream>
+
+
+Matrix::Matrix(std::string matrix_name, int dimension_x, int dimension_y): 
+  matrix_name {matrix_name},
+  dim_x{dimension_x}, 
+  dim_y{dimension_y} {
+
+  this->matrix = new float*[dim_y];
+  for (int i = 0; i < dim_y; i++) {
+    this->matrix[i] = new float[dim_x];
+  }
+}
+
+Matrix::Matrix(std::string matrix_name, int dimension_x, int dimension_y,
+               std::vector<std::vector<float>> arr)
+    : Matrix(matrix_name, dimension_x, dimension_y) {
+  for (int i = 0; i < dim_y; i++) {
+    bool fill_zeros_row = (i >= arr.size());
+    for (int j = 0; j < dim_x; j++) {
+      bool fill_zeros_cell = (j >= arr[i].size());
+      if (fill_zeros_row || fill_zeros_cell) {
+        matrix[i][j] = 0.0f;
+      } else {
+        matrix[i][j] = arr[i][j];
+      }
+    }
+  }
+
+}
+
+Matrix::~Matrix(){
+  for (int i = 0; i < dim_y; i++) {
+    delete[] this->matrix[i];
+  }
+  delete[] this->matrix;
+}
+
+Matrix::Matrix(const Matrix& src): 
+  Matrix(src.matrix_name, src.dim_x, src.dim_y) {
+  this->fill_from(src);
+  
+}
+
+float* Matrix::operator[](int index) { 
+  return this->matrix[index];
+}
+
+Matrix& Matrix::operator=(const Matrix& src) {
+  
+  if (&src != this) {
+    this->~Matrix();
+    this->fill_from(src);
+  }
+  return *this;
+}
+
+void Matrix::display() {
+  for (int i = 0; i < dim_y; i++) {
+    for (int j = 0; j < dim_x; j++) {
+      std::cout << matrix[i][j] << '\t';
+    }
+    std::cout << std::endl;
+  }
+
+}
+
+void Matrix::fill_from(const Matrix& src) {
+  for (int i = 0; i < this->dim_y; ++i) {
+    for (int j = 0; j < this->dim_x; ++j) {
+      this->matrix[i][j] = src.matrix[i][j];
+    }
+  }
+}
---
+++ Matrix/Matrix/utils/matrix/Matrix.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class Matrix {
+ public:
+  Matrix(std::string matrix_name = "Matrix", int dimension_x = 1,
+         int dimension_y = 1);
+  Matrix(std::string matrix_name, int dimension_x, int dimension_y,
+         std::vector<std::vector<float>> arr);
+  ~Matrix();
+
+  Matrix(const Matrix& src);
+  inline int get_x_dimension() const { return dim_x; }
+  inline int get_y_dimension() const { return dim_y; }
+  inline std::string get_matrix_name() const { return matrix_name; }
+
+  float* operator[](int index);
+  Matrix& operator=(const Matrix& src);
+
+  void display();
+
+ private:
+  float** matrix{nullptr};
+  int dim_x, dim_y;
+
+  std::string matrix_name;
+
+  void fill_from(const Matrix& src);
+
+};
